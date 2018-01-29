fa2d159abf69bfa9611570801797d38855f48c98
felix <felix@ffelix.eti.br>
2018-01-29 17:59:15 -0200

    continuando com objetos

--- 06-objeto/06.01-oo.js
+++ 06-objeto/06.01-oo.js
@@ -14,3 +14,14 @@ objeto = {
 }
 
 objeto.processamento()
+
+/*
+  Princípios importantes:
+    1. Abstração - pegar um objeto do mundo real e traduzir para dentro do sistema
+    2. Encapsulamento -  juntar o programa em partes, o mais isoladas possível.
+    A ideia é tornar o software mais flexível, fácil de modificar e de criar 
+    novas implementações.
+    3. Herança (prototype) - "animal" -> "mamífero" -> "humano"
+    4. Polimorfismo - múltiplas formas; a partir de uma variável mais genérica
+    é possível atribuir heranças.
+*/
